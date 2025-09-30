0f2455470a74f9835bc10766c32146d5a6d7705e
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-09-30 18:47:21 +0500

    adding search form in project

--- src/Components/CarSearch.jsx
+++ src/Components/CarSearch.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeSearchTerm } from '../Store'
 
 function CarSearch() {
+    const dispatch = useDispatch()
+    const searchTerm = useSelector((state) => {
+        return state.cars.searchTerm
+    })
+
+    const handleSearchTermChange = (e) => {
+        dispatch(changeSearchTerm(e.target.value))
+    }
   return (
     <div>
-      car search
+        <h3>My Cars</h3>
+        <label>Search</label>
+        <input
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+        />
     </div>
   )
 }
